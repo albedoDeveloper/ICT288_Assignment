b20c15abde08991e7248bdf67706174dbca71d7f
Robert Valentic <robert_val1@hotmail.com>
2020-05-06 20:15:31 +0800

    Merge pull request #15 from albedoDeveloper/InputtingHorn
    Inputting horn

--- Assets/Kye_Stuff/BirdFlocker.cs
+++ Assets/Kye_Stuff/BirdFlocker.cs
@@ -8,6 +8,7 @@ public class BirdFlocker : MonoBehaviour {
     public GameObject train;
     public List<GameObject> birds;
     public float speed;
+    [SerializeField] private Firebox _firebox = null;
 
     [SerializeField]
     private horn horn = null;
@@ -17,7 +18,7 @@ public class BirdFlocker : MonoBehaviour {
     {
         if (collider.tag == "Train" && !hasFlown)
         {
-            train.GetComponent<PathCreation.Examples.PathFollower>().speed = 0;
+            GetComponent<Collider>().enabled = false;
 
             if (horn.hornPosition == 1)
             {
@@ -28,12 +29,12 @@ public class BirdFlocker : MonoBehaviour {
                         bird.GetComponent<Rigidbody>().velocity = new Vector3(Random.Range(-5f, 5.0f), Random.Range(1f, 5f), Random.Range(-5f, 5f)) * speed;
                         hasFlown = true;
                     }
-
                 }
-
             }
-
+            else
+            {
+                _firebox.SetTemperatute(0);
+            }
         }
     }
-
 }
