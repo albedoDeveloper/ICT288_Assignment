047f5893d249185ab82155b4cd9181be31e3d114
Tranquility7 <49786405+Tranquility7@users.noreply.github.com>
2020-06-20 23:57:24 +0800

    LightsLevel2
    derp

---
+++ Assets/Scripts/BossBarrelParent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossBarrelParent : MonoBehaviour
+{
+
+
+    private Vector3 fpsCharacter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fpsCharacter = GameObject.FindGameObjectWithTag("FPSCharacter").transform.position;
+        fpsCharacter = new Vector3(fpsCharacter.x - 40, transform.position.y, fpsCharacter.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, fpsCharacter) > 100f)
+            transform.position = Vector3.MoveTowards(transform.position, fpsCharacter, 5 * Time.deltaTime);
+
+    }
+
+}
---
+++ Assets/Scripts/Cannon.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+
+    public GameObject barrel = null;
+    [SerializeField]
+    private bool leftCannon = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ShootBarrel());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator ShootBarrel()
+    {
+        float randomRange = 0;
+        var pos = Vector3.zero;
+
+        for (int i = 0; i < 100 && this.isActiveAndEnabled; i++)
+        {
+            randomRange = Random.Range(2f, 5.5f);
+            yield return new WaitForSeconds(randomRange);
+
+            if(leftCannon)
+            {
+                pos = new Vector3(transform.position.x + 5, transform.position.y, transform.position.z - 13);
+                Instantiate(barrel, pos, transform.rotation);
+
+            }
+
+            else
+            {
+                pos = new Vector3(transform.position.x - 5, transform.position.y + 1, transform.position.z - 13);
+                Instantiate(barrel, pos, transform.rotation);
+            }
+
+            gameObject.GetComponent<AudioSource>().Play();
+
+        }
+
+      
+    }
+
+}
