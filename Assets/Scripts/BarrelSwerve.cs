6a788debf552a7916ba03ad834c0724a0984d15c
albedoDeveloper <kyehorbury@msn.com>
2020-06-20 12:37:28 +0800

    Fixed AI and collission
    cmon no merge conflicts please

--- Assets/Scripts/BarrelSwerve.cs
+++ Assets/Scripts/BarrelSwerve.cs
@@ -12,12 +12,14 @@ public class BarrelSwerve : MonoBehaviour
     private Vector3 newPosition;
     private Vector3 defaultRotation = Vector3.zero;
 
-    [SerializeField]
-    private int force = 1000;
+    private int force = 5;
 
     private bool shouldRealign = true;
 
     private float randomRange = 0;
+    private float rightJetty = 950f;
+    private float leftJetty = 900f;
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -55,30 +57,20 @@ public class BarrelSwerve : MonoBehaviour
 
             if (randomRange <= 1.5)
             {
-                newPosition = new Vector3(938, transform.position.y, transform.position.z - 30);
+                newPosition = new Vector3(leftJetty, transform.position.y, transform.position.z - 30);
             }
 
             else
             {
-                newPosition = new Vector3(969.17f, transform.position.y, transform.position.z - 30);
+                newPosition = new Vector3(rightJetty, transform.position.y, transform.position.z - 30);
             }
 
             transform.LookAt(newPosition);
             defaultRotation = transform.eulerAngles;
 
-
             shouldRealign = false;
             
         }
-
-
-        //Vector3 lTargetDir = newPosition - transform.position;
-
-        //lTargetDir.y = 0.0f;
-        //transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(lTargetDir), force * Time.deltaTime);
-
-        // transform.LookAt(newPosition);
-        //_rb.AddForce(transform.forward * Time.deltaTime * force);
 
         transform.position = Vector3.MoveTowards(transform.position, newPosition, 15 * Time.deltaTime);
 
@@ -89,7 +81,21 @@ public class BarrelSwerve : MonoBehaviour
         if(Vector3.Distance(newPosition, transform.position) < 2)
             shouldRealign = true;
 
+        rightJetty += 2f * Time.deltaTime;
+        leftJetty += 2f * Time.deltaTime;
+
     }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+    
+        if (collision.gameObject.tag == "barrel")
+        {
+            Physics.IgnoreCollision(collision.gameObject.GetComponent<Collider>(), gameObject.GetComponent<Collider>());
+        }
+    }
+
 
     public void TakeDamage(int amount)
     {
