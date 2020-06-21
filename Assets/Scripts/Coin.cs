dcc905274316ff252b14f55fabd2c6ffb1be86bd
Robert Valentic <robert_val1@hotmail.com>
2020-06-21 12:23:11 +0800

    Merged master into this

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public GameObject coinManager;
+    public float rotationSpeed;
+
+
+    Transform myTransform;
+    Vector3 myEuler = new Vector3(0,0,1);
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTransform = GetComponent<Transform>();
+        coinManager = FindObjectOfType<CoinManager>().gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotate();
+    }
+
+    void Rotate()
+    {
+        transform.Rotate(myEuler, rotationSpeed);
+    }
+
+    public void CollectCoin()
+    {
+        coinManager.GetComponent<AudioSource>().Play();
+        Destroy(this.gameObject);
+    }
+
+
+
+}
