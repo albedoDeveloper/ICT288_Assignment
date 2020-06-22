b4fc35ce537b9f19721eba3365beffb1f1c2a793
Tranquility7 <49786405+Tranquility7@users.noreply.github.com>
2020-06-22 18:14:24 +0800

    added water to all scenes
    both PC and VR toggled

---
+++ Assets/PlatformCheckForWater.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformCheckForWater : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _VRWater;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            _VRWater.GetChild(0).transform.gameObject.SetActive(true);
+        }
+        else
+        {
+            _VRWater.GetChild(1).transform.gameObject.SetActive(true);
+        }
+    }
+}
